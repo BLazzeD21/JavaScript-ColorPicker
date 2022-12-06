3cf4ade739c55ef8d536d1a771e1332dade432a0
BLazzeD21 <blazzed21@gmail.com>
2022-12-06 13:50:37 +0300

    Add selected pixel counter

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 const img = new Image();
 const canvas = document.getElementById('canvas');
 const selectedColor = document.getElementById('selected-color');
+const counterPixelsOnClick = document.getElementById('pixels');
+const allPixels = document.getElementById('all-pixels');
 
 img.crossOrigin = '';
 
@@ -17,11 +19,27 @@ img.addEventListener('load', () => {
 });
 
 function pick(event, destination) {
+    let PixelCounter = 0;
     const bounding = canvas.getBoundingClientRect();
     const x = event.clientX - bounding.left;
     const y = event.clientY - bounding.top;
     const pixel = ctx.getImageData(x, y, 1, 1);
-    const data = pixel.data;
+    let data = pixel.data;
+    for (let i = 1; i <= canvas.width; i++) {
+        for (let j = 1; j <= canvas.height; j++) {
+            const pixelArray = ctx.getImageData(i, j, 1, 1);
+            let pixelColor = pixelArray.data;
+            if(data[0] === pixelColor[0] && data[1] === pixelColor[1] && data[2] === pixelColor[2] && data[3] === pixelColor[3]) {
+                PixelCounter += 1;
+                console.log(pixelColor);
+            }
+        }
+    }
+    console.log(`${PixelCounter}px - rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`);
+
+    allPixels.innerHTML = `Pixels on image: ${canvas.width * canvas.height}`;
+    counterPixelsOnClick.innerHTML = `${PixelCounter}px - rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+
 
     const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
     destination.style.color = rgba;
